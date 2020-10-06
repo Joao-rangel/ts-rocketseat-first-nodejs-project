68a429e006a6fe1dd97a1f70b47cb60f967a9af4
Joao Rangel <joao.vran@gmail.com>
2020-10-05 21:53:03 -0300

    appointments post creation

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,9 +1,22 @@
 import { Router } from 'express';
+import { uuid } from 'uuidv4';
 
 const appointmentsRouter = Router();
 
+const appointments = []; // enquanto não associa ao db
+
 appointmentsRouter.post('/', (request, response) => {
-  return response.json({ message: 'hello world!' });
+  const { provider, date } = request.body;
+
+  const appointment = {
+    id: uuid(),
+    provider,
+    date,
+  };
+
+  appointments.push(appointment);
+
+  return response.json(appointments);
 });
 
 export default appointmentsRouter;
