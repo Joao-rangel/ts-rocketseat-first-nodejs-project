a3152cae6fd3847af7ce8c0143d8b181c22f46f5
Joao Rangel <joao.vran@gmail.com>
2020-10-06 21:56:48 -0300

    checkAppontmentSchedule creation

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
@@ -8,10 +9,22 @@ const appointments = []; // enquanto não associa ao db
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
+  const parsedDate = startOfHour(parseISO(date));
+
+  const checkAppontmentSchedule = appointments.find(appointment =>
+    isEqual(appointment, parsedDate),
+  );
+
+  if (checkAppontmentSchedule) {
+    return response
+      .status(400)
+      .json({ message: 'This schedule has already been scheduled' });
+  }
+
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   };
 
   appointments.push(appointment);
