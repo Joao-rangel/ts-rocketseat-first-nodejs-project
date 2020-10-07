18274b82210f1c9470b08708f23c602a4326fd52
Joao Rangel <joao.vran@gmail.com>
2020-10-07 08:21:17 -0300

    refactoring Appointments Repository

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,31 +1,26 @@
 import { Router } from 'express';
-import { startOfHour, parseISO, isEqual } from 'date-fns';
-import Appointment from '../models/Appointments';
+import { startOfHour, parseISO } from 'date-fns';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
-
-const appointments: Appointment[] = []; // TO DO add db
+const appointmentsRepository = new AppointmentsRepository();
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
   const parsedDate = startOfHour(parseISO(date));
 
-  const checkAppontmentSchedule = appointments.find(appointment =>
-    isEqual(appointment.date, parsedDate),
-  );
+  const checkAppontmentSchedule = appointmentsRepository.findByDate(parsedDate);
 
   if (checkAppontmentSchedule) {
     return response
       .status(400)
-      .json({ message: 'This schedule has already been scheduled' });
+      .json({ message: 'This schedule is already booked' });
   }
 
-  const appointment = new Appointment(provider, parsedDate);
+  const appointment = appointmentsRepository.create(provider, parsedDate);
 
-  appointments.push(appointment);
-
-  return response.json(appointments);
+  return response.json(appointment);
 });
 
 export default appointmentsRouter;
